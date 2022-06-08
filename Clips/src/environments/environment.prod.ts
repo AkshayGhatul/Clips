117f93f27649f409348e434a608663574c250279
Akshay Ghatul <akshay.ghatul@trigensoft.com>
2022-06-08 10:30:52 +0530

    Integrate firebase with Angular

--- Clips/src/environments/environment.prod.ts
+++ Clips/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCjLIaq83-_7D4C0i_hgph-bJ_izPqsFOM",
+    authDomain: "clips-1524f.firebaseapp.com",
+    projectId: "clips-1524f",
+    storageBucket: "clips-1524f.appspot.com",
+    messagingSenderId: "462236895446",
+    appId: "1:462236895446:web:4520e4381dd7eda6a3a177"
+  },
 };
--- Clips/src/environments/environment.ts
+++ Clips/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCjLIaq83-_7D4C0i_hgph-bJ_izPqsFOM",
+    authDomain: "clips-1524f.firebaseapp.com",
+    projectId: "clips-1524f",
+    storageBucket: "clips-1524f.appspot.com",
+    messagingSenderId: "462236895446",
+    appId: "1:462236895446:web:4520e4381dd7eda6a3a177"
+  },
 };
 
 /*
